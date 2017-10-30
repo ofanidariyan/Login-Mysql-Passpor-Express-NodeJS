f816317331f7ef8711339169b1194741392050af
Ofani Dariyan <ofan@skyshi.com>
2017-10-30 19:51:39 +0700

    Update DB name sample

--- configDB.js
+++ configDB.js
@@ -1,10 +1,10 @@
 // config/database.js
 module.exports = {
-    'makekoneksi': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
-    },
-	'database': 'nodejsuser',
-    'users_table': 'user_register'
+  'makekoneksi': {
+    'host': 'localhost',
+    'user': 'root',
+    'password': ''
+  },
+  'database': 'simple-register',
+  'users_table': 'user_register'
 };
